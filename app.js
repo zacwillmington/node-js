d3fae07c5b29981b3fe5b161f84e692a22448283
zacwillmington <zac.willmington@gmail.com>
2019-03-23 12:11:53 -0700

    Added nodemon server and pkj.josn.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Hello world');
+    res.end('Hello world node app');
 });
 
-server.listen(3000);
+server.listen(3000); 
