1ff4a032908456ab192d761a26f08357aa911717
zacwillmington <zac.willmington@gmail.com>
2019-03-23 12:36:24 -0700

    run filename reader from fs module.

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.get('/', (req, res) =>{
-    res.send("Some text");
-});
+// app.get('/', (req, res) =>{
+//     res.send("Some text");
+// });
 
-app.listen(3000);
+// app.listen(3000);
+
+    const fs = require('fs')
+
+    const fileName = 'target.txt';
+    fs.readFile(fileName, (err, data) => {
+        if(err) {
+            console.log(err);
+        }
+            console.log(data.toString());
+    })
+
+    console.log("node js")
